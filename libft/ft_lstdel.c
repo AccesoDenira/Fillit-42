c5296d253929844464baf1ca82acc1970ed05553
Thomas GIRAUD <thgiraud@e2r5p12.42.fr>
2016-12-28 16:31:55 +0100

    LIB

---
+++ libft/ft_lstdel.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thgiraud <thgiraud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/17 12:30:21 by thgiraud          #+#    #+#             */
+/*   Updated: 2016/11/18 13:01:19 by thgiraud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*lst;
+	t_list	*nxtlst;
+
+	lst = *alst;
+	while (lst)
+	{
+		nxtlst = lst->next;
+		del(lst->content, lst->content_size);
+		free(lst);
+		lst = nxtlst;
+	}
+	*alst = NULL;
+}
